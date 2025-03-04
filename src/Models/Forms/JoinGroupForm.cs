f41c28d0cba4c876ede0ab503c663cee09c895ad
太卜『符玄』 <fuxuan001@foxmail.com>
2025-03-04 21:06:47 +0800

    Delete src directory

--- src/Models/Forms/JoinGroupForm.cs
+++
@@ -1,9 +0,0 @@
-
-namespace Herta.Models.Forms.JoinGroupForm;
-
-public sealed class JoinGroupForm
-{
-    public int GroupId { get; set; }
-    public int UserId { get; set; }
-    public string? JoinAbout { get; set; } // 加入的理由
-}
